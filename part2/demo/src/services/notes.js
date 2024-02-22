e22640aeeed966b9f1cf2dfd379886ef77dce875
insv <insv23@outlook.com>
2024-02-22 15:25:46 +0800

    part2: notes add proxy

--- part2/demo/src/services/notes.js
+++ part2/demo/src/services/notes.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // const baseUrl = "http://localhost:3001/notes";
-const baseUrl = "http://localhost:3001/api/notes";
+// const baseUrl = "http://localhost:3001/api/notes";
+const baseUrl = "/api/notes";
 
 // const getAll = () => {
 //   return axios.get(baseUrl).then((response) => response.data);
